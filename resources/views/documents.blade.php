2d6e234d6f068d318d36d63e000d1d453f054ecc
ixistic <teeravipark@gmail.com>
2015-12-22 22:48:26 +0900

    edit some file

--- resources/views/documents.blade.php
+++ resources/views/documents.blade.php
@@ -9,7 +9,7 @@
     <div class="col-xs-12">
       <div class="row">
         <div class="col-xs-6">
-          <a href="/upload"><button class="btn btn-primary" type="button">Add Document</button></a>
+          <a href="{{ route('documents-add') }}"><button class="btn btn-primary" type="button">Add Document</button></a>
         </div>
         <div class="custom-search-input">
           <div class="input-group col-xs-6">
