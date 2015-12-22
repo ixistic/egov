2060e6e97cc509bfaa2e774671346589ba86ab3e
Earth <saritsuriya@hotmail.com>
2015-12-23 03:03:06 +0900

    Map with backend

--- resources/views/document/detail.blade.php
+++ resources/views/document/detail.blade.php
@@ -26,38 +26,55 @@
       <hr class="divider"/>
       <div class="row">
         <div class="col-xs-4">
-          <b>Description</b>
+          <b>Status</b>
         </div>
-        <div class="col-xs-8">
-          {{ $documents->description }}
+        @if ($documents->status == "approved")
+        <div class="col-xs-8 status-app">
+          @elseif ($documents->status == "pre-request")
+          <div class="col-xs-8 status-pre">
+            @elseif ($documents->status == "declined")
+            <div class="col-xs-8 status-dec">
+              @else ($documents->status == "deleted")
+              <div class="col-xs-8 status-del">
+                @endif
+                {{ $documents->status }}
+              </div>
+            </div>
+            <hr class="divider"/>
+            <div class="row">
+              <div class="col-xs-4">
+                <b>Description</b>
+              </div>
+              <div class="col-xs-8">
+                {{ $documents->description }}
+              </div>
+            </div>
+            <hr class="divider"/>
+            <div class="row">
+              <div class="col-xs-4">
+                <b>Feedback</b>
+              </div>
+              <div class="col-xs-8"></div>
+            </div>
+            <br>
+            <div class="well">
+              <strong>Boss</strong><br>
+              <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <small>22/12/2015</small>
+              </blockquote>
+            </div>
+            <br>
+            <div class="text-center">
+              <a href="/documents/edit/{{ $documents->id }}" class="btn btn-warning btn-lg">
+                <span class="glyphicon glyphicon-pencil"></span>Edit
+              </a>
+              <a href="/documents/delete/{{ $documents->id }}" class="btn btn-danger btn-lg" style="margin-left">
+                <span class="glyphicon glyphicon-trash"></span>Delete
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-      <hr class="divider"/>
-      <div class="row">
-        <div class="col-xs-4">
-          <b>Feedback</b>
-        </div>
-        <div class="col-xs-8"></div>
-      </div>
-      <br>
-      <div class="well">
-        <strong>Boss</strong><br>
-        <blockquote>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-          <small>22/12/2015</small>
-        </blockquote>
-      </div>
-      <br>
-      <div class="text-center">
-        <a href="/documents/edit/{{ $documents->id }}" class="btn btn-warning btn-lg">
-          <span class="glyphicon glyphicon-pencil"></span>Edit
-        </a>
-        <a href="/documents/delete/{{ $documents-->id }}" class="btn btn-danger btn-lg" style="margin-left">
-          <span class="glyphicon glyphicon-trash"></span>Delete
-        </a>
-      </div>
-    </div>
-  </div>
-</div>
 
-@endsection
+      @endsection
--- resources/views/document/edit.blade.php
+++ resources/views/document/edit.blade.php
@@ -6,56 +6,80 @@
   <h1>Document Detail</h1>
   <div class="row border-row" style="margin-top: 3vh;">
     <div class="col-xs-12">
-      <div class="row">
-        <div class="col-xs-4">
-          <b>Name</b>
+      <form role="form" method="POST" action="{{ route('documents-edit-post',$documents->id) }}" enctype="multipart/form-data">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="row">
+          <div class="col-xs-4">
+            <b>Name</b>
+          </div>
+          <div class="col-xs-8">
+            <input class="col-md-6" type="text" name="name" id="name" value="{{ $documents->name }}">
+          </div>
         </div>
-        <div class="col-xs-8">
-          <input class="col-md-6" type="text" name="name" id="name" value="{{ $documents->name }}">
+        <hr class="divider"/>
+        <div class="row">
+          <div class="col-xs-4">
+            <b>File</b>
+          </div>
+          <div class="col-xs-8">
+            <input type="file" name="file" id="file">
+          </div>
         </div>
-      </div>
-      <hr class="divider"/>
-      <div class="row">
-        <div class="col-xs-4">
-          <b>File</b>
+        <hr class="divider"/>
+        <div class="row">
+          <div class="col-xs-4">
+            <b>Status</b>
+          </div>
+          <div class="col-xs-8">
+            @if ($documents->status == "approved")
+            <div class="status-app">
+            @elseif ($documents->status == "pre-request")
+            <div class="status-pre">
+            @elseif ($documents->status == "declined")
+            <div class="status-dec">
+            @else ($documents->status == "deleted")
+            <div class="status-del">
+            @endif
+            {{ $documents->status }}
+            </div>
+          </div>
         </div>
-        <div class="col-xs-8">
-          <input type="file" name="file" id="file">
+        <hr class="divider"/>
+        <div class="row">
+          <div class="col-xs-4">
+            <b>Description</b>
+          </div>
+          <div class="col-xs-8">
+            <textarea type="text" name="name" id="name">{{ $documents->description }}</textarea>
+          </div>
         </div>
-      </div>
-      <hr class="divider"/>
-      <div class="row">
-        <div class="col-xs-4">
-          <b>Description</b>
+        <hr class="divider"/>
+        <div class="row">
+          <div class="col-xs-4">
+            <b>Feedback</b>
+          </div>
+          <div class="col-xs-8"></div>
         </div>
-        <div class="col-xs-8">
-          <textarea type="text" name="name" id="name">{{ $documents->description }}</textarea>
+        <br>
+        <div class="well">
+          <strong>Boss</strong><br>
+          <blockquote>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+            <small>22/12/2015</small>
+          </blockquote>
         </div>
-      </div>
-      <hr class="divider"/>
-      <div class="row">
-        <div class="col-xs-4">
-          <b>Feedback</b>
+        <br>
+        <div class="text-center">
+          <button type="submit" href="/documents/edit/post" class="btn btn-success btn-lg">
+            <span class="glyphicon glyphicon-pencil"></span>Save
+          </button>
+          <a href="/documents/delete/{{ $documents->id }}" class="btn btn-danger btn-lg" style="margin-left">
+            <span class="glyphicon glyphicon-trash"></span>Delete
+          </a>
         </div>
-        <div class="col-xs-8"></div>
-      </div>
-      <br>
-      <div class="well">
-        <strong>Boss</strong><br>
-        <blockquote>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-          <small>22/12/2015</small>
-        </blockquote>
-      </div>
-      <br>
-      <div class="text-center">
-        <a href="/documents/edit/post" class="btn btn-success btn-lg">
-          <span class="glyphicon glyphicon-pencil"></span>Save
-        </a>
-        <a href="/documents/delete/{{ $documents->id }}" class="btn btn-danger btn-lg" style="margin-left">
-          <span class="glyphicon glyphicon-trash"></span>Delete
-        </a>
-      </div>
+      </form>
     </div>
   </div>
 </div>
+
+@endsection
