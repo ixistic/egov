1ae03db63c5f471f11eb4ecc548983554cf72a04
Earth <saritsuriya@hotmail.com>
2015-12-22 20:02:55 +0900

    Add upload page.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,7 @@
           <!-- Authentication Links -->
           @if (Auth::guest())
           <li><a href="/login">Login</a></li>
-          <li><a href="/register">Register</a></li>
+          
           @else
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,14 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container spark-screen">
+<div class="container">
   <div class="row">
-    <div class="col-md-10 col-md-offset-1">
-      <form action="upload.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-      </form>
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-heading">Upload document</div>
+        <div class="panel-body">
+            <form role="form" method="POST" action="{{ url('/upload') }}">
+              <input type="hidden" name="_token" value="{{ csrf_token() }}">
+              <div class="row">
+                <label class="col-md-3" for="fileName">Name</label>
+                <input class="col-md-6" type="text" name="fileName" id="fileName">
+              </div>
+              <div class="row">
+                <label class="col-md-3" for="fileToUpload">File</label>
+                <input class="col-md-6" type="file" name="fileToUpload" id="fileToUpload">
+              </div>
+              <div class="row">
+                <label class="col-md-3" for="fileDesc">File description</label>
+                <textarea class="col-md-6 file-desc"type="text-area" name="fileDesc" id="fileDesc"></textarea>
+              </div>
+              <input class="col-md-6 col-md-offset-3 btn btn-primary" type="submit" value="Upload file" name="submit">
+            </form>
+          </div>
+        </div>
     </div>
   </div>
 </div>
