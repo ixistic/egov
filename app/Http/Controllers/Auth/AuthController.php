1ea8a6c0be60e480a6b416a070f0f4591971ca19
ixistic <teeravipark@gmail.com>
2015-12-23 16:16:41 +0900

    fix bug

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/document';
+    protected $redirectTo = '/documents';
 
     /**
      * Create a new authentication controller instance.
