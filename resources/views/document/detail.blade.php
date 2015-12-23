3dd0512d9b80835ba1990113e6d4d3b759249f34
ixistic <teeravipark@gmail.com>
2015-12-23 15:57:18 +0900

    fix indent

--- resources/views/document/detail.blade.php
+++ resources/views/document/detail.blade.php
@@ -36,73 +36,73 @@
           <b>Status</b>
         </div>
         @if ($document->status == "approved")
-        <div class="col-xs-8 status-app">
-          @elseif ($document->status == "pre-request")
+          <div class="col-xs-8 status-app">
+        @elseif ($document->status == "pre-request")
           <div class="col-xs-8 status-pre">
-            @elseif ($document->status == "declined")
-            <div class="col-xs-8 status-dec">
-              @else ($document->status == "deleted")
-              <div class="col-xs-8 status-del">
-                @endif
-                {{ $document->status }}
-              </div>
-            </div>
-            <hr class="divider"/>
-            <div class="row">
-              <div class="col-xs-4">
-                <b>Description</b>
-              </div>
-              <div class="col-xs-8">
-                {{ $document->description }}
-              </div>
-            </div>
-            <hr class="divider"/>
-            <div class="row">
-              <div class="col-xs-4">
-                <b>Feedback</b>
-              </div>
-              <div class="col-xs-8"></div>
-            </div>
-            <br>
-            <div class="well">
-              <strong>Boss</strong><br>
-              <blockquote>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                <small>22/12/2015</small>
-              </blockquote>
-            </div>
-            <form class="form-horizontal"role="form" method="POST" action="{{ route('documents-post') }}" enctype="multipart/form-data">
-              <input type="hidden" name="_token" value="{{ csrf_token() }}">
-              <div class="row">
-                <div class="col-xs-12">
-                    <textarea class="form-control" rows="5" placeholder="Enter feedback"></textarea>    
-                </div>
-              </div>
-            
-
-            <br>
-            @if ($user->is_boss == 0)
-            <div class="text-center">
-              <a href="/documents/edit/{{ $document->id }}" class="btn btn-warning btn-lg">
-                <span class="glyphicon glyphicon-pencil"></span>Edit
-              </a>
-              <a href="/documents/delete/{{ $document->id }}" class="btn btn-danger btn-lg" style="margin-left">
-                <span class="glyphicon glyphicon-trash"></span>Delete
-              </a>
-            </div>
-            @else
-            <div class="text-center">
-                <button type="submit" class="btn btn-success btn-lg">
-                    <span class="glyphicon glyphicon-ok"></span> &nbsp;&nbsp;Approve 
-                </button>
-                <a href="#" class="btn btn-danger btn-lg" style="margin-left">
-                    <span class="glyphicon glyphicon-remove"></span>  &nbsp;&nbsp;Decline 
-                </a>
-            </div>
-            @endif
-          </form>
+        @elseif ($document->status == "declined")
+          <div class="col-xs-8 status-dec">
+        @else ($document->status == "deleted")
+          <div class="col-xs-8 status-del">
+        @endif
+          {{ $document->status }}
           </div>
+      </div>
+      <hr class="divider"/>
+      <div class="row">
+        <div class="col-xs-4">
+          <b>Description</b>
+        </div>
+        <div class="col-xs-8">
+          {{ $document->description }}
         </div>
       </div>
-
-      @endsection
+      <hr class="divider"/>
+      <div class="row">
+        <div class="col-xs-4">
+          <b>Feedback</b>
+        </div>
+        <div class="col-xs-8"></div>
+      </div>
+      <br>
+      <div class="well">
+        <strong>Boss</strong><br>
+        <blockquote>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+          <small>22/12/2015</small>
+        </blockquote>
+      </div>
+      @if ($user->is_boss == 0)
+        <form class="form-horizontal"role="form" method="POST" action="{{ route('documents-post') }}" enctype="multipart/form-data">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <div class="text-center">
+            <a href="/documents/edit/{{ $document->id }}" class="btn btn-warning btn-lg">
+              <span class="glyphicon glyphicon-pencil"></span>Edit
+            </a>
+            <a href="/documents/delete/{{ $document->id }}" class="btn btn-danger btn-lg" style="margin-left">
+              <span class="glyphicon glyphicon-trash"></span>Delete
+            </a>
+          </div>
+        </form>
+      @else
+        <form class="form-horizontal"role="form" method="POST" action="{{ route('documents-post') }}" enctype="multipart/form-data">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <div class="row">
+            <div class="col-xs-12">
+                <textarea class="form-control" rows="5" placeholder="Enter feedback"></textarea>
+            </div>
+          </div>
+          <br>
+          <div class="text-center">
+            <button type="submit" class="btn btn-success btn-lg">
+                <span class="glyphicon glyphicon-ok"></span> &nbsp;&nbsp;Approve
+            </button>
+            <a href="#" class="btn btn-danger btn-lg" style="margin-left">
+                <span class="glyphicon glyphicon-remove"></span>  &nbsp;&nbsp;Decline
+            </a>
+          </div>
+        </from>
+      @endif
+    </div>
+  </div>
+</div>
+@endsection
